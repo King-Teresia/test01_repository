fa284d0df658afb582f25ead03eebfc6088e0513
King-Teresia <wangxinaction@163.com>
2023-02-07 14:18:01 +0800

    忘了commit了

---
+++ test01.js
@@ -0,0 +1,10 @@
+
+
+
+window.addEventListener('load', function (params) {
+    let tp = document.querySelector('button')
+    tp.addEventListener('click', function (params) {
+        console.log(123);
+    })
+
+})
